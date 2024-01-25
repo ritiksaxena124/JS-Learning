4b2bf11d938ec8911a6b6e171a6df3dc502b3741
Ritik Saxena <ritiksaxena124@gmail.com>
2024-01-25 15:26:25 +0530

    page 3 done
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- Project_08/script.js
+++ Project_08/script.js
@@ -1,15 +1,14 @@
 const page1 = document.querySelector("#page1");
 const cursor = document.querySelector("#cursor");
-
 gsap.registerPlugin(ScrollTrigger);
+
 function onLoad() {
-    LocomotiveScroll();
+    LocomotiveScrollAnimation();
     cursorAnimation();
     page2Animation();
 }
 
-function LocomotiveScroll() {
-
+function LocomotiveScrollAnimation() {
     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
     const locoScroll = new LocomotiveScroll({
@@ -19,7 +18,7 @@ function LocomotiveScroll() {
     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
     locoScroll.on("scroll", ScrollTrigger.update);
 
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
     ScrollTrigger.scrollerProxy("#main", {
         scrollTop(value) {
             return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
@@ -30,6 +29,7 @@ function LocomotiveScroll() {
         // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
         pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
     });
+
 
     // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
@@ -72,10 +72,9 @@ function page2Animation() {
         scrollTrigger: {
             trigger: "#page2",
             scroller: "#main",
-            markers: true,
-            start: "top 77%",
-            end: "top 18%",
-            scrub: true
+            start: "top 50%",
+            end: "top 15%",
+            scrub: 3
         }
     })
 }
