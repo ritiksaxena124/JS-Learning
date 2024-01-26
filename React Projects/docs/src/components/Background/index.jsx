2fe2ec7a209abff7ad840c5176ea3e3705e53272
Ritik Saxena <ritiksaxena124@gmail.com>
2024-01-26 23:50:03 +0530

    react project added
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

---
+++ React Projects/docs/src/components/Background/index.jsx
@@ -0,0 +1,9 @@
+function Background() {
+    return (
+        <div className="w-full h-screen bg-zinc-800">
+            <h1 className="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] text-9xl font-bold text-zinc-900 tracking-tight">Docs.</h1>
+        </div>
+    )
+}
+
+export default Background;
